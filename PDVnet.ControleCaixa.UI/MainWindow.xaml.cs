e57a9e1eecd219e97d8b9e855e15d66fccd79c80
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-07-29 14:06:35 -0300

    Criacao do metodo Valor_PreviewTextInput para validar somente valores

--- PDVnet.ControleCaixa.UI/MainWindow.xaml.cs
+++ PDVnet.ControleCaixa.UI/MainWindow.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System.Text;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace PDVnet.ControleCaixa.UI;
 
 /// <summary>
@@ -19,5 +12,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Valor_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        if (sender is not TextBox textBox)
+        {
+            return;
+        }
+
+        string novoTexto = textBox.Text.Insert(
+            textBox.SelectionStart,
+            e.Text);
+
+        Regex regex = new(@"^\d*(,\d{0,2})?$");
+
+        e.Handled = !regex.IsMatch(novoTexto);
     }
 }
