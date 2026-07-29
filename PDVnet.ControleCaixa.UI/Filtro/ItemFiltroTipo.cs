49676f126c4c0e2774441f99271956130d575d99
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-07-29 13:55:23 -0300

    Criando a classe ItemFiltroTipo na camada UI

---
+++ PDVnet.ControleCaixa.UI/Filtro/ItemFiltroTipo.cs
@@ -0,0 +1,11 @@
+﻿using PDVnet.ControleCaixa.Model.Enums;
+
+namespace PDVnet.ControleCaixa.UI.Filtro
+{
+    public sealed class ItemFiltroTipo
+    {
+        public string Descricao { get; set; } = string.Empty;
+
+        public TipoMovimentacao? Tipo { get; set; }
+    }
+}
