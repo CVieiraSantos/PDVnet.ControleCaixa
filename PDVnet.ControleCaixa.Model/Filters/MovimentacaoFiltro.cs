dace923d41a41bd523a7bce47b0c870abf8ab5f9
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-07-29 13:54:35 -0300

    Criacao da classe MovimentacaoFiltro

---
+++ PDVnet.ControleCaixa.Model/Filters/MovimentacaoFiltro.cs
@@ -0,0 +1,15 @@
+﻿using PDVnet.ControleCaixa.Model.Enums;
+
+namespace PDVnet.ControleCaixa.Model.Filters
+{
+    public class MovimentacaoFiltro
+    {
+        public DateTime? DataInicial { get; set; }
+
+        public DateTime? DataFinal { get; set; }
+
+        public TipoMovimentacao? Tipo { get; set; }
+
+        public string? Categoria { get; set; }
+    }
+}
