bcd439770ca607062296a601c0007e03f895c9f7
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-08-10 23:13:34 -0300

    Criando a regra de negocio do valor minimo configuravel na classe ParametroCaixaService

---
+++ PDVnet.ControleCaixa.Business/Services/ParametroCaixaService.cs
@@ -0,0 +1,31 @@
+﻿using PDVnet.ControleCaixa.Business.Exceptions;
+using PDVnet.ControleCaixa.Business.Interfaces;
+using PDVnet.ControleCaixa.Data.Repositories.Interfaces;
+
+namespace PDVnet.ControleCaixa.Business.Services
+{
+    public class ParametroCaixaService : IParametroCaixaService
+    {
+        private readonly IParametroCaixaRepository _parametroCaixaRepository;
+
+        public ParametroCaixaService(IParametroCaixaRepository parametroCaixaRepository)
+        {
+            _parametroCaixaRepository = parametroCaixaRepository;
+        }
+
+        public async Task AtualizarSaldoMinimoAsync(decimal saldoMinimo)
+        {
+            if(saldoMinimo <= 0)
+            {
+                throw new ValidacaoException("O saldo mínimo deve ser maior que zero.");
+            }
+            
+            await _parametroCaixaRepository.AtualizarSaldoMinimoAsync(saldoMinimo);
+        }
+
+        public async Task<decimal> ObterSaldoMinimoAsync()
+        {
+            return await _parametroCaixaRepository.ObterSaldoMinimoAsync();
+        }
+    }
+}
