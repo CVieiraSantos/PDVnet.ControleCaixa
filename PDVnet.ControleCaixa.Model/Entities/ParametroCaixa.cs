2bf8ccf4700a6f171fdbb43fa1e98f22cb0f63dd
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-08-10 23:20:30 -0300

    Criando a model ParametroCaixa

---
+++ PDVnet.ControleCaixa.Model/Entities/ParametroCaixa.cs
@@ -0,0 +1,9 @@
+﻿namespace PDVnet.ControleCaixa.Model.Entities
+{
+    public class ParametroCaixa
+    {
+        public int Id { get; set; }
+
+        public decimal SaldoMinimoAlerta { get; set; }
+    }
+}
