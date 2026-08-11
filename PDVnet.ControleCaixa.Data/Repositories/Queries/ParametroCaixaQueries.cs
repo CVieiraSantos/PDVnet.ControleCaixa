aff68783828190f0ba63ce75da00106a2034975e
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-08-10 23:15:09 -0300

    Criando a query para o parametro de saldo minimo

---
+++ PDVnet.ControleCaixa.Data/Repositories/Queries/ParametroCaixaQueries.cs
@@ -0,0 +1,15 @@
+﻿namespace PDVnet.ControleCaixa.Data.Repositories.Queries
+{
+    public class ParametroCaixaQueries
+    {
+        public const string ObterSaldoMinimo = @"
+        SELECT TOP 1
+            SaldoMinimoAlerta
+        FROM dbo.ParametroCaixa;";
+
+        public const string AtualizarSaldoMinimo = @"
+        UPDATE dbo.ParametroCaixa
+        SET SaldoMinimoAlerta = @SaldoMinimoAlerta
+        WHERE Id = 1;";
+    }
+}
