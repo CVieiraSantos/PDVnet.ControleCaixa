8353712cd6d183e28457f93ebe9ba5e4396d197f
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-08-10 23:14:09 -0300

    Criando a interface IParametroCaixaRepository

---
+++ PDVnet.ControleCaixa.Data/Repositories/Interfaces/IParametroCaixaRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PDVnet.ControleCaixa.Data.Repositories.Interfaces {
+    public interface IParametroCaixaRepository
+    {
+        Task<decimal> ObterSaldoMinimoAsync();
+
+        Task AtualizarSaldoMinimoAsync(decimal saldoMinimo);
+    }
+}
